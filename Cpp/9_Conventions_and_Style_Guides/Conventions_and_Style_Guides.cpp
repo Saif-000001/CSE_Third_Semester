f3ad63941648763f3258032a88a6c118e302aafb
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-09-16 00:56:40 +0600

    Conventions and Style Guides

---
+++ Cpp/9_Conventions_and_Style_Guides/Conventions_and_Style_Guides.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    // This code will insult the user
+
+    int slice;
+    std::cout << "You fifty how many pieces of pizza you eat ? : ";
+    std::cin >> slice;
+    std::cout << "You have " << slice << " slice pizza." << std::endl;
+    return 0;
+}
