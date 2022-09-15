e3f0199ef9b5d44f48cb1b1559ae3d68f28a759f
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-09-16 00:49:49 +0600

    using input, output

---
+++ Cpp/7_Using_Variables_with_cout/Using_Variables_with_cout.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    int slice = 10;
+    std::cout << "We have " << slice << " slice pizza." << std::endl;
+    printf("%i", slice);
+    return 0;
+}
---
+++ Cpp/8_User_Input_with_cin/User_Input_with.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int slice;
+    std::cin >> slice;
+    std::cout << "We have " << slice << " slice pizza." << std::endl;
+
+    // printf("%i", slice);
+    return 0;
+}
