ecb6d522b07155e1fe7920095614786aeb9462bf
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-09-16 19:33:02 +0600

    Char Data

---
+++ Cpp/18_char_Data_Type/char_Data_Type.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    // char x = 127;
+    unsigned char x = 128;
+    std::cout << x << std::endl;
+    std::cout << (int)x << std::endl;
+
+    std::cout << "Hello \"There\"  ";
+}
