e7f7aad07b0595072800b45e9679875e1a992b8c
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-09-18 23:04:54 +0600

    Class across

---
+++ Cpp/96_Class_Across_Files/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "user.h"
+
+int main()
+{
+    User user;
+    std::cin >> user;
+    std::cout << user << std::endl;
+    return 0;
+}
---
+++ Cpp/96_Class_Across_Files/user.h
@@ -0,0 +1,34 @@
+// #include <iostream>
+#ifndef USER_H
+#define USER_H
+class User
+{
+    static int user_count;
+    std::string status = "Gold";
+
+public:
+    static int get_user_count();
+    std::string first_name;
+    std::string last_name;
+
+    // Encapsulate
+    // Getters
+    std::string get_status();
+
+    // setters
+    void set_status(std::string status);
+
+    // constructors
+    User();
+
+    User(std::string first_name, std::string last_name, std::string status);
+
+    // Destructors
+    ~User();
+
+    friend void output_status(User user);
+    friend std::ostream &operator<<(std::ostream &output, const User user);
+    friend std::istream &operator>>(std::istream &input, User &user);
+};
+
+#endif
