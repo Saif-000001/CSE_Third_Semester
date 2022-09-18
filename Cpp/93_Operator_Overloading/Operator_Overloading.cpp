7ea75b9a716f0e51e2c4ff552960983da1155c7f
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-09-18 22:05:39 +0600

    Operator overloading

---
+++ Cpp/93_Operator_Overloading/Operator_Overloading.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+class Position
+{
+public:
+    int x = 10;
+    int y = 20;
+
+    Position operator+(Position pos)
+    {
+        Position new_pos;
+        new_pos.x = x + pos.x;
+        new_pos.y = y + pos.y;
+        return new_pos;
+    }
+
+    bool operator==(Position pos)
+    {
+        if (x == pos.x && y == pos.y)
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+// std::ostream &operator<<(std::ostream output, Position use)
+// {
+//     return output;
+// }
+
+int main()
+{
+    Position pos1, pos2;
+
+    Position pos3 = pos1 + pos2;
+    std::cout << pos3.x << "\t" << pos3.y << std::endl;
+    // pos1.x = 30;
+
+    if (pos1 == pos2)
+    {
+        std::cout << "They are the same!" << std::endl;
+        std::cout << "That I really tacos" << std::endl;
+        std::cout << "Pizza" << std::endl;
+    }
+
+    Position use;
+    // std::cout << use << std::endl;
+    return 0;
+}
