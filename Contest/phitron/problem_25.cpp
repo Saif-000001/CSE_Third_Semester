ac7752b5999633d1588ccda879a78f949da37c06
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-09-26 21:37:19 +0600

    replay

--- Contest/phitron/problem_25.cpp
+++ Contest/phitron/problem_25.cpp
@@ -5,10 +5,12 @@ int main()
     int a, b, m = 1;
     std::cout << "Enter number a and b : ";
     std::cin >> a >> b;
+
     int l = a;
+
     if (a < b)
     {
-        for (int i = 1; i < b / a; i++)
+        for (int i = 0; i < b / a; i++)
         {
             std::cout << m << " ";
             for (int j = m + 1; j <= l; j++)
@@ -18,6 +20,6 @@ int main()
             std::cout << std::endl;
             m += a;
             l += a;
-        }
+                }
     }
 }
