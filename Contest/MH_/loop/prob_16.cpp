ef5072169cb5ba38d18798dcc21ad663d53f9148
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-10-06 17:40:13 +0600

    prob_16

---
+++ Contest/MH_/loop/prob_16.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    int n, sum = 0;
+    std::cin >> n;
+    for (int i = 2; i < n; i++)
+    {
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                break;
+            }
+            else if (i == j + 1)
+            {
+                std::cout << i << std::endl;
+                sum++;    // 24
+                sum += i; // 1082
+            }
+        }
+    }
+    std::cout << sum << std::endl;
+}
