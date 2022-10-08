0d8a56e76360a3b76fa35905d41f50a07668854c
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-10-08 19:54:53 +0600

    Day_8

---
+++ Contest/target/Day_8_DaDictionaries_and_Maps.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+
+int main()
+{
+    std::map<std::string, std::string> phoneBook;
+
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        std::string phone;
+        std::string book;
+        std::cin >> phone;
+        std::cin >> book;
+
+        phoneBook[phone] = book;
+    }
+
+    std::map<std::string, std::string>::iterator it;
+    std::string query;
+
+    while (std::cin >> query)
+    {
+
+        it = phoneBook.find(query);
+        if (it != phoneBook.end())
+        {
+            std::cout << it->first << " - " << it->second << std::endl;
+        }
+        else
+        {
+            std::cout << "Not found " << std::endl;
+        }
+    }
+}
