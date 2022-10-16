bf0da96d3ca16aa75b5d2ed49703d397d8e942b3
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-10-16 16:43:53 +0600

    Overloading Unary Minus

--- OOP/OOP_BOOKs/Chapter_7/unary_operator.cpp
+++ OOP/OOP_BOOKs/Chapter_7/unary_operator.cpp
@@ -7,7 +7,40 @@ class Space
     int z;
 
 public:
-    void getData(int a, int b, int c)
-    {
-    }
+    void getData(int a, int b, int c);
+    void display();
+    void operator-();
 };
+
+void Space::getData(int a, int b, int c)
+{
+    x = a;
+    y = b;
+    z = c;
+}
+
+void Space::display()
+{
+    std::cout << "x = " << x << std::endl;
+    std::cout << "x = " << y << std::endl;
+    std::cout << "x = " << z << std::endl;
+}
+
+void Space::operator-()
+{
+    x = -x;
+    y = -y;
+    z = -z;
+}
+
+int main()
+{
+    Space s;
+    s.getData(2, -4, 6);
+    // s.display();
+    s.operator-();
+
+    -s;
+    s.operator-();
+    s.display();
+}
