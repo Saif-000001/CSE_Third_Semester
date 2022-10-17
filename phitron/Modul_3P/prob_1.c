755c7c5b92bc4ac61b3a5b4e43feaec1e3dcd6dc
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-10-18 00:22:36 +0600

    prob_1

---
+++ phitron/Modul_3P/prob_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hi there /\.");
+    printf("\tI hope you are doing greet and practicing well.\n");
+    printf("Now this is a simple task you need to do.");
+    printf("\tJust care for the indention.\n");
+    printf("Show 100% of sincerity");
+}
