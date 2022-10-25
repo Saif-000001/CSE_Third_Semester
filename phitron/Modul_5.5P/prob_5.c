3e2f0de7f70a322bfda134bd3028d8e0c4261691
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-10-25 23:16:26 +0600

    prob_5

---
+++ phitron/Modul_5.5P/prob_5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         printf("%d ", i);
+    //     }
+    // }
+
+    int i = 1;
+    while (i <= n)
+    {
+        if (n % i == 0)
+        {
+            printf("%d ", i);
+        }
+        i++;
+    }
+}
