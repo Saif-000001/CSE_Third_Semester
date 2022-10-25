5f6e93955b3a12f5e9cd1d7ec88555e30948a43b
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-10-25 18:25:44 +0600

    Watermelon

---
+++ Contest/CSES/Missing_Number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100];
+    int n;
+    scanf("%d ", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int temp = n + 1;
+    for (int i = 0; i < n; i++)
+    {
+    }
+}
