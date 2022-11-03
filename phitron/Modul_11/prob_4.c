4bb60feff64f7c3349d2062c1a1abd6389244ab2
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-11-04 00:22:42 +0600

    prob_4

---
+++ phitron/Modul_11/prob_4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = largest - arr[i];
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
