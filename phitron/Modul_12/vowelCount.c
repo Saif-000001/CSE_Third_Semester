c920c99f8a044299958f50df081ee9f900eca8f3
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-11-05 23:07:52 +0600

    vowelCount

--- phitron/Modul_12/vowelCount.c
+++ phitron/Modul_12/vowelCount.c
@@ -2,4 +2,17 @@
 
 int main()
 {
+    char sent[100];
+    // scanf("%s", sent);
+    // gets(sent);
+    fgets(sent, sizeof(sent), stdin);
+    int count = 0;
+    for (int i = 0; sent[i] != '\0'; i++)
+    {
+        if (sent[i] == 'a' || sent[i] == 'e' || sent[i] == 'i' || sent[i] == 'o' || sent[i] == 'u')
+        {
+            count++;
+        }
+    }
+    printf("Number of vowel counter : %d", count);
 }
