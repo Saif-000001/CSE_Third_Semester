17250254a5f283b341e967523bac2b286cee12fb
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-11-05 22:48:13 +0600

    Modul 12

---
+++ phitron/Modul_12/string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // char name[15];
+    // scanf("%s", name);
+
+    char name[15] = {"Farhan Feroz"};
+    // printf("The name is %s ", name);
+    name[4] = 'i';
+    // name[5] = '\0';
+    name[12] = 'X';
+    name[13] = '\0';
+
+    // printf("%c", name[12]);
+    printf("%s", name);
+    return 0;
+}
