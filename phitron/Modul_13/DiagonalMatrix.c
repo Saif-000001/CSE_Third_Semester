1917d2078624ebf852d7b14e523b8af7660f82fe
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-11-07 16:01:05 +0600

    Diagonal Matrix

---
+++ phitron/Modul_13/DiagonalMatrix.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+    int row, col;
+    scanf("%d%d", &row, &col);
+    int arr[row][col];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    if (row == col)
+    {
+        int ans = 1;
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+                if (arr[i][j] != 0)
+                {
+                    ans = 0;
+                }
+            }
+        }
+        if (ans == 1)
+        {
+            printf("Diagonal");
+        }
+        else
+        {
+            printf("Not diagonal");
+        }
+    }
+    else
+    {
+        printf("Not diagonal");
+    }
+}
