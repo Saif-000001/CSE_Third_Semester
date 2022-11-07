bf2a40467b4e03fb9681131735d778839767c374
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-11-07 16:08:31 +0600

    Diagonal matrix secondary

--- phitron/Modul_13/DiagonalMatrix1.c
+++ phitron/Modul_13/DiagonalMatrix1.c
@@ -1,1 +1,48 @@
 #include <stdio.h>
+
+int main()
+{
+    int row, col;
+    scanf("%d%d", &row, &col);
+
+    int arr[row][col];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    if (row == col)
+    {
+        int ans = 1;
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (i + j == row - 1)
+                {
+                    continue;
+                }
+                if (arr[i][j] != 0)
+                {
+                    ans = 0;
+                }
+            }
+        }
+        if (ans == 1)
+        {
+            printf("Diagonal");
+        }
+        else
+        {
+            printf("Not diagonal");
+        }
+    }
+    else
+    {
+        printf("Not diagonal");
+    }
+}
