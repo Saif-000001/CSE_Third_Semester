f19a84e75102d221e14978aff415965d7dff0003
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-11-25 00:48:53 +0600

    add

---
+++ phitron/Modul_23/stringSwap.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+void stringSwap(char str[]);
+
+int main()
+{
+    char str[100];
+
+    gets(str);
+
+    stringSwap(str);
+
+    return 0;
+}
+
+void stringSwap(char str[])
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] += 32;
+        }
+        else if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] -= 32;
+        }
+    }
+    printf("%s", str);
+}
