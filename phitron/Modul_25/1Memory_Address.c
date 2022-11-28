7a3b68e32f57dabcc08a9aafe7f2f9329ca247ad
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-11-28 12:05:28 +0600

    Memory Address

---
+++ phitron/Modul_25/1Memory_Address.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    // int a, b;
+    long long int a, b;
+    // float a, b;
+    // char a, b;
+    // a = 25.25;
+    printf("%p\n", &a);
+    printf("%p", &b);
+
+    // int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // for (int i = 0; i < sizeof(arr); i++)
+    // {
+    //     printf("%p\n", &arr[i]);
+    // }
+
+    // char arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // for (int i = 0; i < sizeof(arr); i++)
+    // {
+    //     printf("%p\n", &arr[i]);
+    // }
+}
