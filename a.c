8045f8bb44477c5b2c7745bf36ca05a178de1913
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-12-06 23:40:22 +0600

    add

--- a.c
+++ a.c
@@ -443,13 +443,75 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main()
+// {
+
+//     int *ptr;
+//     ptr = (int *)malloc(1 * sizeof(int));
+//     printf("%d", *ptr);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// struct Course
+// {
+//     char name[20];
+//     int price;
+// };
+
+// int main()
+// {
+//     struct Course c1 = {"C Programming", 5000};
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// struct st
+// {
+//     int x;
+//     int ar[5];
+// };
+
+// int main()
+// {
+//     printf("%d", sizeof(struct st));
+//     return 0;
+// }
+
+// #include <stdio.h>
+// struct st
+// {
+//     char x;
+//     int ar[6];
+//     int a;
+//     double b;
+// };
+
+// int main()
+// {
+//     printf("%d", sizeof(struct st));
+//     return 0;
+// }
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+
+struct Name
+{
+    char str[20];
+};
+
 int main()
 {
+    struct Name st1, st2;
+    strcpy(st1.str, "ProgrammingHero");
+    st2 = st1;
+    st1.str[0] = 'p';
 
-    int *ptr;
-    ptr = (int *)malloc(1 * sizeof(int));
-    printf("%d", *ptr);
+    printf("%s", st2.str);
+
     return 0;
 }
