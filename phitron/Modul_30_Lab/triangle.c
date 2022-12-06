1314cd0db2c3e336d4ea58067569a4f401d2792c
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-12-07 01:19:32 +0600

    triangle

---
+++ phitron/Modul_30_Lab/triangle.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define PI 3.1416
+
+struct Point
+{
+    float x;
+    float y;
+};
+
+struct Triangle
+{
+    struct Point A;
+    struct Point B;
+    struct Point C;
+};
+
+struct Circale
+{
+    struct Point l;
+};
+struct Point center(struct Triangle t)
+{
+    struct Point ans;
+    ans.x = (t.A.x + t.B.x + t.C.x) / 3;
+    ans.y = (t.A.y + t.B.y + t.C.y) / 3;
+    return ans;
+}
+
+int main()
+{
+    struct Point p1 = {1, 1};
+    struct Point p2 = {2, 2};
+    struct Point p3 = {3, 3};
+
+    struct Triangle P = {p1, p2, p3};
+
+    struct Point cen = center(P);
+
+    printf("Center distance (%.2f , %.2f)\n", cen.x, cen.y);
+
+    return 0;
+}
