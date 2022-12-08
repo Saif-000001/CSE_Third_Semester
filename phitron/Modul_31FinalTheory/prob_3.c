33ef6d1c46db20169d0d1c11ea6066f71fe7bc77
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-12-09 02:05:34 +0600

    prob 5

--- phitron/Modul_31FinalTheory/prob_3.c
+++ phitron/Modul_31FinalTheory/prob_3.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 
-void passByValue(int a, int b)
+void passByReference(int *a, int *b)
 {
-    int temp = a;
-    a = b;
-    b = temp;
-    printf("After the swapping value a = %d , b = %d\n", a, b); // swapping in this
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    printf("After the swapping value a = %d , b = %d\n", *a, *b); // swapping in this
 }
 int main()
 {
     int a, b;
     scanf("%d%d", &a, &b);
     printf("Before the swapping value a = %d , b = %d\n", a, b);
-    passByValue(a, b); // this is not change in main function pass by value
+    passByReference(&a, &b); // this is swapping in main function pass by reference
     printf("After the swapping value a = %d , b = %d\n", a, b);
 
     return 0;
 }
+// void passByValue(int a, int b)
+// {
+//     int temp = a;
+//     a = b;
+//     b = temp;
+//     printf("After the swapping value a = %d , b = %d\n", a, b); // swapping in this
+// }
+// int main()
+// {
+//     int a, b;
+//     scanf("%d%d", &a, &b);
+//     printf("Before the swapping value a = %d , b = %d\n", a, b);
+//     passByValue(a, b); // this is not change in main function pass by value
+//     printf("After the swapping value a = %d , b = %d\n", a, b);
+
+//     return 0;
+// }
